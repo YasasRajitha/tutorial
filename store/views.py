6b08a11ed7d7c302328b049b06d08d755251b531
yasraj6969 <yasas15955@gmail.com>
2023-11-25 12:48:08 +0530

    handles the DoesNotExist exception

--- store/views.py
+++ store/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Product
 from .serializers import ProductSerializers
 
@@ -13,6 +14,9 @@ def product_list(request):
 
 @api_view()
 def product_detail(request,id):
-    product = Product.objects.get(pk=id)
-    serializer = ProductSerializers(product)
-    return Response(serializer.data)
+    try:
+        product = Product.objects.get(pk=id)
+        serializer = ProductSerializers(product)
+        return Response(serializer.data)
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
